4df017ae32bec9f6a262a0144dbcbefb62b26a51
jm117818 <jmazurek.kontakt@gmail.com>
2023-05-03 16:46:46 +0200

    CRUD requests

---
+++ src/main/java/com/example/movielibrary/Movie.java
@@ -0,0 +1,16 @@
+package com.example.movielibrary;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    private int id;
+    private String movie;
+    private int rating;
+
+
+}
