0899ab0195ae09618ffb0ef1ad6556db976e4267
jm117818 <jmazurek.kontakt@gmail.com>
2023-05-03 13:13:14 +0200

    Project template

---
+++ src/main/java/com/example/movielibrary/MovielibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.movielibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovielibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovielibraryApplication.class, args);
+	}
+
+}
